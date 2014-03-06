3c50050d4acc0441ec1829f9cfbc1eb2d3ca889e
Brandon Davis <bdavis@redspin.net>
2014-03-06 00:52:13 -0500

    Removed header - added recent projects

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 include 'header.php'; 
 echo $header;
 ?>
-<p>
-<center>
-<img src='/images/image.gif' width=750 height=120/>
-</center>
+<p><br>
 My name is Brandon Davis, and I'm a computer science student at The University of North Carolina at Chapel Hill.  I like to struggle with Java, PHP, and Python, and sometimes I even end up with runnable programs.  I am particularly interested in Linux and networking.  I also tinker with electronics, and I'm pretty fond of the Arduino platform. <a href='/me.php'>More About Me.</a>
 </p>
 <h1>Projects</h1>
 <p>
-I currently operate RedSpinMC, a public Minecraft server.  More info on that at <a href='http://rsmc.tk'>rsmc.tk</a>.  I'm also working on some small projects, including a <a href='http://bukkit.org'>Bukkit</a> Economy Plugin.  I just bought a <a href='http://www.raspberrypi.org/'>Raspberry Pi</a> to tinker with, and it runs this website.
+I currently operate RedSpinMC, a public Minecraft server.  More info on that at <a href='http://rsmc.tk'>rsmc.tk</a>.  I'm also working on some small projects, including <a href="/blog/laserspacepong.php">Laser Space Pong</a> and a tool called <a href="https://github.com/suBDavis/FlashRemote">FlashRemote</a> which sends real-time key presses from a mobile device to any computer.  I just bought a <a href='http://www.raspberrypi.org/'>Raspberry Pi</a> to tinker with, and it runs this website.
 </p>
 <h1>Blog Posts</h1>
 <h3><span class="bloghead"><a href="/blog/arduino-binary-clock.php">Arduino Binary Clock </a></span><date> Aug 18, 2013</date></h3>
